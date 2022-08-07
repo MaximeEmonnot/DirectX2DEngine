49a2107ae44c8f8884f5e31c914be2411f5556f5
MaximeEmonnot <maxime.emonnot41@gmail.com>
2022-08-08 00:50:27 +0200

    Ajout du module Debug (classique, simple classe d'exception)

--- DirectX2DEngine/Main.cpp
+++ DirectX2DEngine/Main.cpp
@@ -1,6 +1,18 @@
+#include "Window.h"
+#include "EngineException.h"
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+	try {
+		while (WND.ProcessMessages())
+		{
 
+		}
+	}
+	catch(...)
+	{
+		SHOW_MESSAGE("DirectX 2D Engine - Unknown Exception", "An unknown exception has been caught.");
+	}
+	
 	return 0;
 }
